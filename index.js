2820abbf06ed1bac76e612fe9a486a1685391580
ballarro <ron.ballard@accenture.com>
2022-08-09 16:05:10 -0700

    Module 1.0 - initialize

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Accenture.SecOps')
 });
 
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Kellyn","lastName":"Beck","email":"kellyn.beck@accenture.com"},
+    {"id": 2,"firstName":"Carl","lastName":"Dukatz","email":"carl.dukatz@accenture.com"},
+    {"id": 3,"firstName":"Stephen","lastName":"Harper","email":"stephen.harper@accenture.com"}
   ]);
 });
 
